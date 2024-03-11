a4c1916b27025a950483204d4ab0e11e2ee38a6c
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2024-03-11 18:34:59 +0100

    -Added user dto

--- src/main/java/com/example/codealongw3/dto/UserDTO.java
+++ src/main/java/com/example/codealongw3/dto/UserDTO.java
@@ -1,4 +1,32 @@
 package com.example.codealongw3.dto;
 
 public class UserDTO {
+
+    public String email;
+    public String password;
+
+    public String voornaam;
+
+    public String tussenvoegsel;
+
+    public String achternaam;
+
+    public String straat;
+
+    public int huisnummer;
+
+    public String postcode;
+
+    public UserDTO(String email, String password, String voornaam, String tussenvoegsel, String achternaam, String straat, int huisnummer, String postcode) {
+        this.email = email;
+        this.password = password;
+        this.voornaam = voornaam;
+        this.tussenvoegsel = tussenvoegsel;
+        this.achternaam = achternaam;
+        this.straat = straat;
+        this.huisnummer = huisnummer;
+        this.postcode = postcode;
+    }
+
+
 }
