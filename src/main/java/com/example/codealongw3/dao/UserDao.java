930bb2a9097a17cb24158c60796d69486c5f77c5
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2024-03-11 18:17:47 +0100

    -Added user update function -Added user delete function -Added get user by id -Added user create function

--- src/main/java/com/example/codealongw3/dao/UserDao.java
+++ src/main/java/com/example/codealongw3/dao/UserDao.java
@@ -1,10 +1,15 @@
 package com.example.codealongw3.dao;
 
+import com.example.codealongw3.dto.UserDTO;
 import com.example.codealongw3.models.CustomUser;
+import jakarta.transaction.Transactional;
 import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserDao {
@@ -17,6 +22,36 @@ public class UserDao {
     public List<CustomUser> getAllUsers(){
         return this.userRepository.findAll();
     }
+
+//    public void updateUser(UserDTO userDTO, Long id){
+//        Optional<CustomUser> user = this.userRepository.findById(id);
+//
+//        if(user.isPresent()){
+//            user.get().setVoornaam(userDTO.voornaam);
+//
+//        }
+//    }
+//
+
+    public CustomUser getUser(long id){
+        Optional<CustomUser> user = this.userRepository.findById(id);
+
+        if(user.isPresent()){
+            return user.get();
+        } else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No user has been found");
+        }
+    }
+
+   @Transactional
+    public void createUser(){
+        CustomUser user = new CustomUser();
+        this.userRepository.save(user);
+   }
+
+   public void deletUserById(Long id){
+        this.userRepository.deleteById(id);
+   }
 
 
 
